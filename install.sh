a9f01642fda3c1e6db3456e6bcfb99407f454145
甬哥kkkyg <90416692+kkkyg@users.noreply.github.com>
2022-03-11 16:05:04 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -161,7 +161,7 @@ install_x-ui() {
     systemctl daemon-reload
     systemctl enable x-ui
     systemctl start x-ui
-echo -e "${green}安装每分种自检x-ui的守护进程${plain}" 
+green "安装每分种自检x-ui的守护进程" 
 sleep 2
 cat>/root/goxui.sh<<-\EOF
 #!/bin/bash
